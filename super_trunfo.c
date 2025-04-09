deea59a87cfe0b641d0e54f8c7d0b8228944e474
eduardolage <lorduduru283@gmail.com>
2025-04-09 23:59:41 +0000

    ccc

--- super_trunfo.c
+++ super_trunfo.c
@@ -10,7 +10,7 @@ int main () {
     char Cidade[30] = "Belo Horizonte";
     float Populacao = 100.000;
     float Area = 10.000;
-    float PIB = 500.000;
+    float PIB = 500.00;
     int Turismo = 50;
 
 
@@ -21,7 +21,7 @@ int main () {
     printf("CODIGO DA CARTA:%s\n", Codigo);
     printf("NOME DA CIDADE:%s\n", Cidade);
     printf("POPULACAO:%.3f\n", Populacao);
-    printf("AREA:%.3fkm²\n", Area);
+    printf("AREA:%fkm²\n", Area);
     printf("PIB:%.6f bilhoes de reais\n", PIB);
     printf("PONTOS TURISTICOS:%d\n", Turismo);
     printf("\n");
@@ -32,14 +32,14 @@ int main () {
     char Cidade2[30] = "Fortaleza";
     float Populacao2 = 60.000;
     float Area2 = 7.000;
-    float PIB2 = 300.000;
+    float PIB2 = 300.00;
     int Turismo2 = 30;
 
     printf("CARTA:%d\n", Carta2);
     printf("ESTADO:%c\n", Estado2);
     printf("CODIGO DA CARTA:%s\n", Codigo2);
     printf("NOME DA CIDADE:%s\n", Cidade2);
-    printf("POPULACAO:%.3f\n", Populacao2);
+    printf("POPULACAO:%f\n", Populacao2);
     printf("AREA:%.3fkm²\n", Area2);
     printf("PIB:%.6f bilhoes de reais\n", PIB2);
     printf("PONTOS TURISTICOS:%d\n", Turismo2);
