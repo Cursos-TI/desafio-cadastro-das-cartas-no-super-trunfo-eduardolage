141fdf5b19c15168840f9c50673ec6126f92c007
eduardolage <lorduduru283@gmail.com>
2025-04-09 23:55:44 +0000

    bh

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,11 +2,12 @@
 
 int main () {
     printf("Desafio Super Trunfo em C\n");
+    //variaveis carta 1
 
     int Carta = 1;
     char Estado = 'A';
     char Codigo[30] = "A01";
-    char Cidade[30] = "BeloHorizonte";
+    char Cidade[30] = "Belo Horizonte";
     float Populacao = 100.000;
     float Area = 10.000;
     float PIB = 500.000;
@@ -23,6 +24,30 @@ int main () {
     printf("AREA:%.3fkm²\n", Area);
     printf("PIB:%.6f bilhoes de reais\n", PIB);
     printf("PONTOS TURISTICOS:%d\n", Turismo);
+    printf("\n");
+
+    int Carta2 = 2;
+    char Estado2 = 'B';
+    char Codigo2[30] = "A02";
+    char Cidade2[30] = "Fortaleza";
+    float Populacao2 = 60.000;
+    float Area2 = 7.000;
+    float PIB2 = 300.000;
+    int Turismo2 = 30;
+
+    printf("CARTA:%d\n", Carta2);
+    printf("ESTADO:%c\n", Estado2);
+    printf("CODIGO DA CARTA:%s\n", Codigo2);
+    printf("NOME DA CIDADE:%s\n", Cidade2);
+    printf("POPULACAO:%.3f\n", Populacao2);
+    printf("AREA:%.3fkm²\n", Area2);
+    printf("PIB:%.6f bilhoes de reais\n", PIB2);
+    printf("PONTOS TURISTICOS:%d\n", Turismo2);
+
+
+   
+
+
 
 
 
