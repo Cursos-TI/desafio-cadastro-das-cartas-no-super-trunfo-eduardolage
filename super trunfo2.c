24c59804bbee445d359903e2595520e23ba975cf
eduardolage <lorduduru283@gmail.com>
2025-06-25 23:53:21 +0000

    dsds

--- super trunfo2.c
+++ super trunfo2.c
@@ -18,11 +18,29 @@ int main () {
     printf("Digite o numero da carta: \n");
     scanf("%d", &Carta);
 
-    printf("Digite a populacao: \n");
+    printf("Digite o nome do Estado: \n");
     scanf("%c", &Estado);
 
     printf("Digite o codigo da carta: \n");
     scanf("%c", &Codigo);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%c", &Cidade);
+
+    printf("Digite a populacao: \n");
+    scanf("%f", &Populacao);
+
+    printf("Digite a area: \n");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite a quantidade de turistas: \n");
+    scanf("%d", &Turismo);
+
+
+
 
 
 
