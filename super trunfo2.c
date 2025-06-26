97d45c7614b4785751abeb3cbd1546ca95e78cdd
eduardolage <lorduduru283@gmail.com>
2025-06-26 00:08:02 +0000

    q q eu to jogando

--- super trunfo2.c
+++ super trunfo2.c
@@ -7,28 +7,19 @@ int main () {
     char Estado;
     char Codigo[30];
     char Cidade[30];
-    float Populacao;
+    int Populacao;
     float Area;
     float PIB;
     int Turismo;
 
-    //int:%d, char:%c, float:%f ou %.(numero de numeros)f (ex:%.2f)
+    //int:%d, char:%c (%s de string na real), float:%f ou %.(numero de numeros)f (ex:%.2f)
 
 
     printf("Digite o numero da carta: \n");
     scanf("%d", &Carta);
 
-    printf("Digite o nome do Estado: \n");
-    scanf("%c", &Estado);
-
-    printf("Digite o codigo da carta: \n");
-    scanf("%c", &Codigo);
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%c", &Cidade);
-
     printf("Digite a populacao: \n");
-    scanf("%f", &Populacao);
+    scanf("%d", &Populacao);
 
     printf("Digite a area: \n");
     scanf("%f", &Area);
@@ -36,7 +27,7 @@ int main () {
     printf("Digite o PIB: \n");
     scanf("%f", &PIB);
 
-    printf("Digite a quantidade de turistas: \n");
+    printf("Digite o numero de pontos turisticos: \n");
     scanf("%d", &Turismo);
 
 
