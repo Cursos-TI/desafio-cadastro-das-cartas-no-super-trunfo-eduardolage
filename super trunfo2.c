b2d7175a36db548af8679068f7727061444eabce
eduardolage <lorduduru283@gmail.com>
2025-07-04 00:28:11 +0000

    segunda pt

--- super trunfo2.c
+++ super trunfo2.c
@@ -5,7 +5,7 @@ int main () {
 
     int Carta, Turismo, Populacao, Carta2,Turismo2, Populacao2;
     char Estado;
-    float Area, PIB, Area2, PIB2;
+    float Area, PIB, Area2, PIB2, PIBPC, PIBPC2, Densidade, Densidade2;
     
     //int:%d, char:%c (%s de string na real), float:%f ou %.(numero de numeros)f (ex:%.2f)
 
@@ -25,6 +25,14 @@ int main () {
     printf("Digite o numero de pontos turisticos da carta 1: \n");
     scanf("%d", &Turismo);
 
+    PIBPC = (PIB / Populacao);
+
+    printf("PIB Per Capita da carta 1:%f\n", PIBPC);
+
+    Densidade = (Area / Populacao);
+
+    printf("Densidade populacional da carta 1:%f\n", Densidade);
+
     printf("Digite o numero da carta 2: \n");
     scanf("%d", &Carta2);
 
@@ -39,6 +47,10 @@ int main () {
 
     printf("Digite o numero de pontos turisticos da carta 2: \n");
     scanf("%d", &Turismo2);
+
+    
+
+
 
 
 
